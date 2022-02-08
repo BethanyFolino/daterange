70849faaa15c34732783a519391bad154d9dcc09
BethanyFolino <bethany.folino@gmail.com>
2022-02-07 17:22:07 -0800

    Tried again using a different method for building date range picker

--- rangepicker/src/App.js
+++ rangepicker/src/App.js
@@ -1,9 +1,17 @@
+import React from 'react';
 import './App.css';
+import DatePicker from './DatePicker/DatePicker';
+
+function onChange(timestamp) {
+  console.log(timestamp);
+}
 
 function App() {
   return (
     <div className="App">
-      
+      <h2>Welcome to Date Range Picker!</h2>
+      <h4>To use, click on the date box below.</h4>
+      <DatePicker onChange={onChange}/>
     </div>
   );
 }
